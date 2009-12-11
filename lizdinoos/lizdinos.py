9aeb516c59ba330d8ab3eb790fbfcc2b9fe00eeb
lizm <lmartinez.bavc@gmail.com>
2009-12-10 19:21:22 -0800

    my first program

---
+++ lizdinoos/lizdinos.py
@@ -0,0 +1,16 @@
+def dino1():
+
+    print "There once was a boy named Joe.\n"
+
+    print "\nThere once was a girl named Monique.\n"
+
+    print "\nWhich one do you choose to be? Monique or Joe? Remember, choose wisely for you cannot go back in this story.\n"
+
+    answer=input()
+
+    if answer == Monique:
+        print "Congrats! You will be Monique for as long as you continue through this story. Quick description of you: You are a girl who loves to stand up for what you believe in, even if at the moment you are speaking your mind may not be appropiate, you don' care. You like to be smart, even if at times it may take you awhile, you achieve. One more thing, you are obsessed with the band called 'The Beatles'.\n"
+        
+        print "\nWould you like to continue on with who you are, or get on with what Joe plays in your life this instant? Alright.\n"
+
+dino1()
